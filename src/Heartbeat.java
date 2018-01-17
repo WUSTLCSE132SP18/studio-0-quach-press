df37a933978adfd256699c93c9ab3127ffbca0f6
Logan P <lpress@eastsideprep.org>
2018-01-17 12:04:08 -0800

    Updated to milliseconds

--- src/Heartbeat.java
+++ src/Heartbeat.java
@@ -3,14 +3,14 @@ public class Heartbeat {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int time = 0;
+		double time = 0;
 		while (true) {
 			try {
 				Thread.sleep(1000);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-			time += 1;
+			time += 1000;
 			System.out.println(time);
 		}
 	}
