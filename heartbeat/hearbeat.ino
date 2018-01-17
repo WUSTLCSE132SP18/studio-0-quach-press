4077a43143f0f39e8c1f7347e291c2b7ad26b983
Stefan Quach <stefantquach@gmail.com>
2018-01-17 13:30:06 -0600

    Finished heartbeat program

---
+++ heartbeat/hearbeat.ino
@@ -0,0 +1,13 @@
+void setup() {
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+}
+int n=0;
+void loop() {
+  digitalWrite(13, HIGH);
+  Serial.println( String(n) + " sec have elapsed");
+  n++;
+  delay(200);
+  digitalWrite(13, LOW);
+  delay(800);
+}
