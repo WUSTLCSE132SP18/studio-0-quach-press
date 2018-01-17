bf854d8566af5a54ebb186ce5cede527bf7caea0
Stefan Quach <stefantquach@gmail.com>
2018-01-17 13:54:03 -0600

    Changed to measure current time

--- heartbeat/heartbeat.ino
+++ heartbeat/heartbeat.ino
@@ -5,9 +5,10 @@ void setup() {
 int n=0;
 void loop() {
   digitalWrite(13, HIGH);
-  Serial.println( String(n) + " sec have elapsed");
-  n++;
+  //Serial.println( String(n) + " sec have elapsed");
+  //n++;
   delay(200);
   digitalWrite(13, LOW);
   delay(800);
+  Serial.println("Current time: " + String(millis()));
 }
